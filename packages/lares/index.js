f496fdc613b17a344a9a01643f9f65ad424d897c
joshu <joshua.fontany@gmail.com>
2026-05-02 10:15:37 -0700

    fix: correct calculation of repoRoot path in lares module, fix build scripts

--- packages/lares/index.js
+++ packages/lares/index.js
@@ -3,4 +3,4 @@ import { dirname } from "node:path";
 
 export const laresRoot = dirname(fileURLToPath(import.meta.url));
 /** Absolute path to the monorepo root (two levels up from packages/lares/). */
-export const repoRoot = dirname(laresRoot);
+export const repoRoot = dirname(dirname(laresRoot));
